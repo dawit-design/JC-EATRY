b4840d22ec2f3a04d1fbb974337cac7c8a007947
dawit-design <dawitawegchow@gmail.com>
2021-12-01 13:37:43 -0500

    api configuration done

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,27 +1,27 @@
-import React, {useState} from 'react';
-import {View, Text, StyleSheet, ScrollView} from 'react-native';
-import SearchBar from '../components/SearchBar'
+import React, { useState } from "react";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import SearchBar from "../components/SearchBar";
 
-const SearchScreen = () =>{
-    const [term, setTerm] = useState('')
+const SearchScreen = () => {
+  const [term, setTerm] = useState("");
 
-    return (
-       <View>
-           <Text>Search Screen</Text>
-           <SearchBar 
-           term={term}
-           onTermChange={setTerm}
-           onTermSubmit={() => console.log('hey')}
-           />
-           <ScrollView>
-                <Text>Cost Effective</Text>
-                <Text>Bit Harder</Text>
-                <Text>Bit Spender</Text>
-           </ScrollView>
-       </View>
-    );
-}
+  return (
+    <View>
+      <Text>Search Screen</Text>
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => console.log("hey")}
+      />
+      <ScrollView>
+        <Text>Cost Effective</Text>
+        <Text>Bit Harder</Text>
+        <Text>Bit Spender</Text>
+      </ScrollView>
+    </View>
+  );
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
 
 export default SearchScreen;
---
+++ src/services/api.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+export default axios.create({
+    baseURL: 'https://api.yelp.com/v3/businesses',
+    headers: {
+        Authorization: 'Bearer allEBgz8Gq24kFgef-uAc23ZQ3eQJCEUvcwN2eTuDwPyPN6BuEh-ZFhGjOGRjeI-cW8TZwqhE9uMpe-zbiY4x-eoNMFtbj6QfY4aHHBchILbDhL1GjqkiUXEtmKlYXYx'
+    }
+})
