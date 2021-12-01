fc93e9552011c28f36c168c1cc8824f9eb3a934d
dawit-design <dawitawegchow@gmail.com>
2021-12-01 13:34:04 -0500

    search bar done

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,12 +2,18 @@ import React from 'react';
 import {View, TextInput, StyleSheet} from "react-native";
 import {Feather} from "@expo/vector-icons"
 
-const SearchBar = () =>{
+const SearchBar = ({term, onTermChange, onTermSubmit}) =>{
     return (
         <View style={styles.backgroundStyle}>
             <Feather name="search" style={styles.iconStyle} />
             <TextInput 
             style={styles.inputStyle}
+            autoCapitalize='none'
+            autoCorrect={false}
+            placeholder="Search"
+            value={term}
+            onChangeText={onTermChange}
+            onEndEditing={onTermSubmit}
             />
         </View>
     );
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
 import SearchBar from '../components/SearchBar'
 
 const SearchScreen = () =>{
+    const [term, setTerm] = useState('')
+
     return (
        <View>
            <Text>Search Screen</Text>
            <SearchBar 
+           term={term}
+           onTermChange={setTerm}
+           onTermSubmit={() => console.log('hey')}
            />
+           <ScrollView>
+                <Text>Cost Effective</Text>
+                <Text>Bit Harder</Text>
+                <Text>Bit Spender</Text>
+           </ScrollView>
        </View>
     );
 }
